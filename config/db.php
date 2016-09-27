eb768e9e34fc0bce09aac804b7ed9e8ad420d250
ianpierreg <ianpierreg@gmail.com>
2016-09-26 22:20:32 -0300

    Inicio do Projeto

---
+++ config/db.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=reserva_exercito',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+];
+
